c1c0affd363d624a01296ac7c3f6b8d91b665483
amitmarko <amitmarkovich92@gmail.com>
2022-01-19 11:14:12 +0200

    Add babel loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const elvenShildRecipe = {
   leatherStrips: 2,
   ironIngot: 1,
   refinedMoostone: 4
+};
+
+const newobject = {
+  ...elvenShildRecipe,
+  amit: 100
 }
 
 console.log(elvenShildRecipe);
+console.log(newobject);
